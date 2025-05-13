0067386cb8ff62d47c20cd17b35a522833c00d98
Skanda <skanda.td@gmail.com>
2025-05-13 15:23:01 +0530

    Init code

---
+++ src/main/java/com/example/springbootpaginationsortingdemo/SpringbootpaginationsortingdemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootpaginationsortingdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootpaginationsortingdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootpaginationsortingdemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springbootpaginationsortingdemo/SpringbootpaginationsortingdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootpaginationsortingdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootpaginationsortingdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
